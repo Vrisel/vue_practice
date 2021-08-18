a71a0cd2a20ec291e2de2beced9c6cfaf575b2db
Vrisel <vrisel.p@gmail.com>
2021-08-18 23:46:30 +0900

    기본 틀 작성

---
+++ src/components/Tarot.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form id="tarotForm">
+      <label for="type">방식</label>
+      <select name="type" id="type">
+        <option value="1">기본</option>
+      </select>
+
+      <label for="question">질문</label>
+      <input type="text" name="question" id="question" />
+    </form>
+    <div id="tarotAnswer"></div>
+  </div>
+</template>
+
+<script>
+import tarot from './scripts/tarot.js'
+
+export default {
+  name: 'Tarot'
+}
+</script>
+
+<style>
+</style>
---
+++ src/scripts/tarot.js
@@ -0,0 +1,8 @@
+let form = document.getElementById('tarotForm');
+
+form.addEventListener('submit', function (e) {
+  // 입력 비활성화
+  // (랜덤)카드 뽑기
+  // 해당 카드 정보 출력
+  // 리셋 버튼 (누르면 카드정보 지우고 입력창 활성화 및 포커스) 생성
+});
