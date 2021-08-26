ecf857da21a55f4c8525dbe57fed64e150aafcb3
Vrisel <vrisel.p@gmail.com>
2021-08-26 17:12:16 +0900

    .js 쓰지 않고 component에서 해결하는 방향으로

--- src/components/Tarot.vue
+++ src/components/Tarot.vue
@@ -1,39 +1,94 @@
 <template>
   <div>
-    <form id="tarot-form" @submit.prevent="tarotQuestion">
-      <label for="type">방식</label>
-      <select name="type" id="type">
-        <option value="1">기본</option>
-      </select>
+    <form @submit.prevent="onSubmit">
+      <label>
+        스프레드
+        <select v-model="spread" ref="spread">
+          <option disabled value="">==스프레드==</option>
+          <option>기본</option>
+        </select>
+      </label>
 
-      <label for="tarot-question">질문</label>
-      <input type="text" v-model="value" id="question" placeholder="질문을 입력하세요." />
-      <button type="submit">질문하기</button>
+      <label>
+        질문
+        <input type="text" placeholder="질문을 입력하세요." ref="question" v-model.lazy.trim="question" :readonly="isAsked"/>
+      </label>
+      <button type="submit" :disabled="isAsked">질문하기</button>
     </form>
-    <div id="tarot-answer"></div>
-    <button type="button" id="tarot-reset" @click.prevent="tarotReset" class="disabled" disabled>다시하기</button>
+
+    <div v-show="isAsked">
+      <p v-html="answer"></p>
+      <button type="button" @click="tarotReset">다시하기</button>
+    </div>
   </div>
 </template>
 
 <script>
-import {tarotQuestion, tarotReset} from '@/scripts/tarot.js'
-
-/* document.addEventListener("DOMContentLoaded", function(){
-  let question = document.getElementById('tarot-form');
-  question.addEventListener('submit', '#tarot-form', function(e) {e.preventDefault(); tarotQuestion();}, false);
-  let reset = document.getElementById('tarot-reset');
-  reset.addEventListener('click', function(e) {e.preventDefault(); tarotReset();}, false);
-}); */
+import tarot from '@/scripts/tarot.js';
 
 export default {
   name: 'Tarot',
+  data() {
+    return {
+      isAsked: false,
+      answer: "",
+      spread: "",
+      cards: [],
+      tarot
+    };
+  },
   methods: {
-    tarotQuestion,
-    tarotReset
+    onSubmit: function() {
+      if (!this.spread) {
+        alert('스프레드 방식을 선택해주세요.');
+        this.$refs.spread.focus();
+      }
+      else if (!this.question) {
+        alert('질문을 입력해주세요.');
+        this.$refs.question.focus();
+      }
+      /* else if (!this.question.endsWith('?')) {
+        alert('질문은 일반적으로 물음표로 끝나지요? :)');
+        this.$refs.question.focus();
+      } */
+      else {
+        this.isAsked = true;
+        this.cards.push(this.getRandomCard());
+        this.answer = `뽑은 카드는 <strong>${Math.random() > 0.5 ? '정' : '역'}방향</strong>의 <strong>${this.cards[0].name} 카드</strong> 입니다.<br/>`;
+        this.answer += `이 카드는 <strong>${this.cards[0].desc}</strong>을(를) 의미합니다.`;
+        //this.$refs.resetBtn.focus(); //v-if나 v-show가 동작하기 이전에 focus를 잡으려고 해서인지 워닝 뜸
+      }
+    },
+    tarotReset: function () {
+      if (window.confirm('다시 하시겠습니까?')) {
+        this.cards = [];
+        this.isAsked = false;
+        this.answer = "";
+        this.question = "";
+        this.$refs.question.focus();
+      }
+    },
+    getRandomCard: function() {
+      let a = ['M', 'c', 's', 'h', 'd'][Math.floor(Math.random() * 5)];
+      let b;
+      if (a === 'M') {
+        b = Math.floor(Math.random() * 22);
+      }
+      else {
+        b = Math.floor(Math.random() * 14) + 1;
+        switch (b) {
+          case 11: b = 'P'; break;
+          case 12: b = 'N'; break;
+          case 13: b = 'Q'; break;
+          case 14: b = 'K'; break;
+          default: break;
+        }
+      }
+      return tarot[a+b];
+    }
   }
 }
 </script>
 
 <style>
-.disabled { display:none; }
 </style>
--- src/scripts/tarot.js
+++ src/scripts/tarot.js
@@ -1,43 +1,392 @@
-export {tarotQuestion, tarotReset};
-
-/* form.addEventListener('submit', function (e) {
-  // 입력 비활성화
-  // (랜덤)카드 뽑기
-  // 해당 카드 정보 출력
-  // 리셋 버튼 (누르면 카드정보 지우고 입력창 활성화 및 포커스) 생성
-}); */
-
-function tarotQuestion(){
-  let form = document.getElementById('tarot-form');
-  let question = document.getElementById('tarot-question');
-  
-  if (!question.value) {
-    window.alert('질문을 입력해주세요.');
-    question.focus();
+export default {
+  "M0": {
+    "number": "Major 0",
+    "name": "The Fool (광대)",
+    "desc": "모험, 무지"
+  },
+  "M1": {
+    "number": "Major 1",
+    "name": "The Magician (마술사)",
+    "desc": "창조, 수완"
+  },
+  "M2": {
+    "number": "Major 2",
+    "name": "The High Priestess (사제)",
+    "desc": "지식, 총명"
+  },
+  "M3": {
+    "number": "Major 3",
+    "name": "The Empress (여제)",
+    "desc": "풍양, 모성"
+  },
+  "M4": {
+    "number": "Major 4",
+    "name": "The Emperor (황제)",
+    "desc": "책임, 부성"
+  },
+  "M5": {
+    "number": "Major 5",
+    "name": "The Hierophant (교황)",
+    "desc": "가르침, 관대함"
+  },
+  "M6": {
+    "number": "Major 6",
+    "name": "The Lovers (연인)",
+    "desc": "연애, 쾌락"
+  },
+  "M7": {
+    "number": "Major 7",
+    "name": "The Chariot (전차)",
+    "desc": "전진, 승리"
+  },
+  "M8": {
+    "number": "Major 8",
+    "name": "The Strength (힘)",
+    "desc": "힘, 용기"
+  },
+  "M9": {
+    "number": "Major 9",
+    "name": "The Hermit (은둔자)",
+    "desc": "탐색, 사려깊음"
+  },
+  "M10": {
+    "number": "Major 10",
+    "name": "Wheel of Fortune (운명의 수레바퀴)",
+    "desc": "기회, 일시적 행운"
+  },
+  "M11": {
+    "number": "Major 11",
+    "name": "Justice (정의)",
+    "desc": "균형, 정당함"
+  },
+  "M12": {
+    "number": "Major 12",
+    "name": "The Hanged Man (매달린 사람)",
+    "desc": "자기희생, 인내"
+  },
+  "M13": {
+    "number": "Major 13",
+    "name": "Death (죽음)",
+    "desc": "격변, 이별"
+  },
+  "M14": {
+    "number": "Major 14",
+    "name": "Temperance (절제)",
+    "desc": "조화, 견실"
+  },
+  "M15": {
+    "number": "Major 15",
+    "name": "The Devil (악마)",
+    "desc": "사심, 속박, 타락"
+  },
+  "M16": {
+    "number": "Major 16",
+    "name": "The Tower (탑)",
+    "desc": "파괴, 파멸"
+  },
+  "M17": {
+    "number": "Major 17",
+    "name": "The Star (별)",
+    "desc": "희망, 동경"
+  },
+  "M18": {
+    "number": "Major 18",
+    "name": "The Moon (달)",
+    "desc": "불안, 애매함, 혼돈"
+  },
+  "M19": {
+    "number": "Major 19",
+    "name": "The Sun (태양)",
+    "desc": "밝은 미래, 만족"
+  },
+  "M20": {
+    "number": "Major 20",
+    "name": "Judgement (심판)",
+    "desc": "부활, 개선"
+  },
+  "M21": {
+    "number": "Major 21",
+    "name": "The World (세계)",
+    "desc": "완성, 완전"
+  },
+  "c1": {
+    "number": "club 1",
+    "name": "지팡이 1",
+    "desc": "창조력, 출발"
+  },
+  "c2": {
+    "number": "club 2",
+    "name": "지팡이 2",
+    "desc": "재산, 장엄함, 영주"
+  },
+  "c3": {
+    "number": "club 3",
+    "name": "지팡이 3",
+    "desc": "확립된 힘, 교역, 사업상의 협력"
+  },
+  "c4": {
+    "number": "club 4",
+    "name": "지팡이 4",
+    "desc": "작업의 완성, 휴식, 평화"
+  },
+  "c5": {
+    "number": "club 5",
+    "name": "지팡이 5",
+    "desc": "치열한 경쟁, 스포츠"
+  },
+  "c6": {
+    "number": "club 6",
+    "name": "지팡이 6",
+    "desc": "승리자, 대뉴스의 도달"
+  },
+  "c7": {
+    "number": "club 7",
+    "name": "지팡이 7",
+    "desc": "용기, 토론, 교섭"
+  },
+  "c8": {
+    "number": "club 8",
+    "name": "지팡이 8",
+    "desc": "활동성, 재빠름"
+  },
+  "c9": {
+    "number": "club 9",
+    "name": "지팡이 9",
+    "desc": "억압된 상황에서의 강함"
+  },
+  "c10": {
+    "number": "club 10",
+    "name": "지팡이 10",
+    "desc": "억압, 너무 많은 재산"
+  },
+  "cP": {
+    "number": "club Page",
+    "name": "지팡이 Page",
+    "desc": "젊은 남성, 충실, 외교사절, 우편"
+  },
+  "cN": {
+    "number": "club Knight",
+    "name": "지팡이 Knight",
+    "desc": "출발, 친숙한 젊은이"
+  },
+  "cQ": {
+    "number": "club Queen",
+    "name": "지팡이 Queen",
+    "desc": "시골의 여성, 친숙한 정적, 존경"
+  },
+  "cK": {
+    "number": "club King",
+    "name": "지팡이 King",
+    "desc": "시골의 남성, 정직, 양심적"
+  },
+  "s1": {
+    "number": "spade 1",
+    "name": "성배 1",
+    "desc": "힘의 승리, 사랑과 증오에 있어서의 커다란 힘"
+  },
+  "s2": {
+    "number": "spade 2",
+    "name": "성배 2",
+    "desc": "균형, 조건부의 조화"
+  },
+  "s3": {
+    "number": "spade 3",
+    "name": "성배 3",
+    "desc": "후퇴, 단절, 슬픔"
+  },
+  "s4": {
+    "number": "spade 4",
+    "name": "성배 4",
+    "desc": "은둔, 회복, 자기치유"
+  },
+  "s5": {
+    "number": "spade 5",
+    "name": "성배 5",
+    "desc": "타락, 폐지, 손실"
+  },
+  "s6": {
+    "number": "spade 6",
+    "name": "성배 6",
+    "desc": "작업을 끝마침, 중개자, 득책"
+  },
+  "s7": {
+    "number": "spade 7",
+    "name": "성배 7",
+    "desc": "기획, 계획"
+  },
+  "s8": {
+    "number": "spade 8",
+    "name": "성배 8",
+    "desc": "구속된 힘 비난, 나쁜 소식"
+  },
+  "s9": {
+    "number": "spade 9",
+    "name": "성배 9",
+    "desc": "실망, 환멸"
+  },
+  "s10": {
+    "number": "spade 10",
+    "name": "성배 10",
+    "desc": "황폐, 고통, 여명"
+  },
+  "sP": {
+    "number": "spade Page",
+    "name": "성배 Page",
+    "desc": "감시, 경계, 스파이, 시험"
+  },
+  "sN": {
+    "number": "spade Knight",
+    "name": "성배 Knight",
+    "desc": "용감함, 격노"
+  },
+  "sQ": {
+    "number": "spade Queen",
+    "name": "성배 Queen",
+    "desc": "정숙하고 슬픔이 많은 여성, 미망인, 상실, 예리한, 눈치 빠른, 결단력 있는"
+  },
+  "sK": {
+    "number": "spade King",
+    "name": "성배 King",
+    "desc": "재판관, 정의, 권위, 명령"
+  },
+  "h1": {
+    "number": "heart 1",
+    "name": "성배 1",
+    "desc": "기쁨, 만족"
+  },
+  "h2": {
+    "number": "heart 2",
+    "name": "성배 2",
+    "desc": "사랑, 우정, 일치"
+  },
+  "h3": {
+    "number": "heart 3",
+    "name": "성배 3",
+    "desc": "풍족함, 행복, 성취, 자유"
+  },
+  "h4": {
+    "number": "heart 4",
+    "name": "성배 4",
+    "desc": "권태, 포식, 뒤섞인 쾌락"
+  },
+  "h5": {
+    "number": "heart 5",
+    "name": "성배 5",
+    "desc": "손실, 별 거 아닌 유산"
+  },
+  "h6": {
+    "number": "heart 6",
+    "name": "성배 6",
+    "desc": "과거를 돌이켜 봄,, 행복, 즐거움"
+  },
+  "h7": {
+    "number": "heart 7",
+    "name": "성배 7",
+    "desc": "환상, 약간의 성공(비 영구적)"
+  },
+  "h8": {
+    "number": "heart 8",
+    "name": "성배 8",
+    "desc": "성공의 방치, 겸손"
+  },
+  "h9": {
+    "number": "heart 9",
+    "name": "성배 9",
+    "desc": "물질적 안녕, 만족"
+  },
+  "h10": {
+    "number": "heart 10",
+    "name": "성배 10",
+    "desc": "만족, 사랑과 우정의 완전함"
+  },
+  "hP": {
+    "number": "heart Page",
+    "name": "성배 Page",
+    "desc": "공부에 힘쓰는 젊은이, 숙고"
+  },
+  "hN": {
+    "number": "heart Knight",
+    "name": "성배 Knight",
+    "desc": "도착, 발전, 제안, 고무(鼓舞)"
+  },
+  "hQ": {
+    "number": "heart Queen",
+    "name": "성배 Queen",
+    "desc": "선량하고 공정한 여성, 행복, 지혜"
+  },
+  "hK": {
+    "number": "heart King",
+    "name": "성배 King",
+    "desc": "공정한 남성, 창조적 지성"
+  },
+  "d1": {
+    "number": "diamond 1",
+    "name": "동전 1",
+    "desc": "완전한 만족, 돈"
+  },
+  "d2": {
+    "number": "diamond 2",
+    "name": "동전 2",
+    "desc": "명람함, 문서에 의한 뉴스/메시지"
+  },
+  "d3": {
+    "number": "diamond 3",
+    "name": "동전 3",
+    "desc": "기예, 거래, 숙련공"
+  },
+  "d4": {
+    "number": "diamond 4",
+    "name": "동전 4",
+    "desc": "소유의 보정, 소유물에 대한 집착, 선물"
+  },
+  "d5": {
+    "number": "diamond 5",
+    "name": "동전 5",
+    "desc": "물질적인 트러블"
+  },
+  "d6": {
+    "number": "diamond 6",
+    "name": "동전 6",
+    "desc": "성공, 선물"
+  },
+  "d7": {
+    "number": "diamond 7",
+    "name": "동전 7",
+    "desc": "금전, 사업, 교역"
+  },
+  "d8": {
+    "number": "diamond 8",
+    "name": "동전 8",
+    "desc": "장인 기질, 준비"
+  },
+  "d9": {
+    "number": "diamond 9",
+    "name": "동전 9",
+    "desc": "물질적인 풍요, 달성"
+  },
+  "d10": {
+    "number": "diamond 10",
+    "name": "동전 10",
+    "desc": "이익, 재산, 가족"
+  },
+  "dP": {
+    "number": "diamond Page",
+    "name": "동전 Page",
+    "desc": "정려근면, 학생"
+  },
+  "dN": {
+    "number": "diamond Knight",
+    "name": "동전 Knight",
+    "desc": "유용, 재산, 책임, 정직"
+  },
+  "dQ": {
+    "number": "diamond Queen",
+    "name": "동전 Queen",
+    "desc": "부, 관대, 안정"
+  },
+  "dK": {
+    "number": "diamond King",
+    "name": "동전 King",
+    "desc": "실제적인 지성, 사업, 성공"
   }
-  else if (!question.value.endsWith('?')) {
-    window.alert('질문은 일반적으로 물음표로 끝나지요? :)');
-    question.focus();
-  }
-  else {
-    question.setAttribute('readonly');
-    let answer = document.getElementById('tarot-answer');
-    answer.innerHTML = `뽑은 카드는 <strong>${Math.random() > 0.5 ? '정' : '역'}방향</strong>의 <strong>${Math.floor(Math.random()*64)}번 카드</strong> 입니다.`;
-  }
-
-  return false;
-}
-
-function tarotReset() {
-  if (window.confirm('다시 하시겠습니까?')) {
-    let answer = document.getElementById('tarot-answer');
-    answer.innerHTML = '';
-
-    let question = document.getElementById('tarot-question');
-    question.value = '';
-    question.removeAttribute('readonly');
-    question.focus();
-  }
-
-  return false;
-}
+};
