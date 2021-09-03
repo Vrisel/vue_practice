17eecfbcd469c1c64d0c4b277dd798f805ed9065
Vrisel <vrisel.p@gmail.com>
2021-09-03 11:43:39 +0900

    전체 레이아웃 관련 조정
    -Header component 분리
    -footer가 페이지 최하단에 위치하도록 style 추가

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
-    <b-navbar
-      variant="light" type="light"
-      toggleable="lg"
-      class="px-4"
-    >
-      <b-navbar-brand href='/home'>Vue-ractice</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/Home">홈</b-nav-item>
-          <b-nav-item to="/tarot">타로</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Header></Header>
     
-    <router-view></router-view>
+    <main>
+      <router-view></router-view>
+    </main>
 
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/layouts/Header.vue';
 import Footer from '@/components/layouts/Footer.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
     Footer
   }
 }
@@ -40,5 +29,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
 }
 </style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <b-navbar
+      variant="light" type="light"
+      toggleable="lg"
+      class="px-4"
+    >
+      <b-navbar-brand href='/home'>Vue-ractice</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/Home">홈</b-nav-item>
+          <b-nav-item to="/tarot">타로</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+</style>
